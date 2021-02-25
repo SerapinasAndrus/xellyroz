6dc86d8950808b9c2d257a953ec1923f7ca235e1
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 12:44:03 +0300

    notes

--- frontend/controllers/NotesController.php
+++ frontend/controllers/NotesController.php
@@ -3,7 +3,11 @@ namespace frontend\controllers;
 
 
 use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 use frontend\models\Notes;
 
 
