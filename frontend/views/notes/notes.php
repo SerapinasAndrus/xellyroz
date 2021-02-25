098b13aee22cfd316e4e4e30a147af4f31a92cf6
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 13:34:37 +0300

    notes

--- frontend/views/notes/notes.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Url;
-?>
-
-<?php foreach($notes as $note):?>
-    <div class='col-lg-4'>
-        <a href="<?=Url::to(['notes/note', 'id' => $note->id])?>"><?=$note->name?></a>
-    </div>
-<?php endForeach;?>
