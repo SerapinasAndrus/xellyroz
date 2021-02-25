8662296f36c6c6128d779a893ebd23b62a86ccd0
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 14:22:29 +0300

    notes

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 
 <div class="wrap">
     <?php
-    if (Yii::$app->user->isGuest){$xelly = 'Гость';}else{$xelly = Yii::$app->user-username;}
+    if (Yii::$app->user->isGuest){$xelly = 'Гость';}else{$xelly = Yii::$app->user->identity->username;}
     NavBar::begin([
         'brandLabel' => $xelly,
         'brandUrl' => Yii::$app->homeUrl,
