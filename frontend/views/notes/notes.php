98904dfcb3a59b91f6ec8c3c542e940504331ffc
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 13:02:28 +0300

    notes

--- frontend/views/notes/notes.php
+++ frontend/views/notes/notes.php
@@ -4,7 +4,6 @@ use yii\helpers\Url;
 
 <?php foreach($notes as $note):?>
     <div>
-        <p><?=$note->id . $note->name?></p>
-        <a href="<?=Url::to(['notes/note', 'id' => $note->id])?>"><?=$note->id?></a>
+        <a href="<?=Url::to(['notes/note', 'id' => $note->id])?>"><?=$note->name?></a>
     </div>
 <?php endForeach;?>
