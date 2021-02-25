bac41f9c0f8ea521002f95ff2a765ba03ddc2e4c
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 12:08:14 +0300

    notes

--- frontend/controllers/NotesController.php
+++ frontend/controllers/NotesController.php
@@ -3,77 +3,20 @@ namespace frontend\controllers;
 
 
 use Yii;
-use yii\base\InvalidArgumentException;
-use yii\web\BadRequestHttpException;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
+use frontend\models\Notes;
 
 
-/**
- * Site controller
- */
+
 class NotesController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
-    /**
-     * Displays homepage.
-     *
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
     public function actionNotes()
     {
-        return $this->render('notes');
+        $notes = Notes::find()->all()->one();
+        return $this->render('notes', [
+            'notes' => $notes
+        ]);
     }
 
 
