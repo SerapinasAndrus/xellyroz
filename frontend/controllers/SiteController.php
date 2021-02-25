f6bb2c2f85c72e51cd6f3200ff0d1c1451aa6533
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-25 14:48:26 +0300

    notes

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -76,12 +76,13 @@ class SiteController extends Controller
     {
     if (Yii::$app->user->isGuest){
         return $this->redirect(['login']);
-    }
+    }else{
                 $userid = Yii::$app->user->id);
                 $notes = Notes::find()->where('userid' => $userid);
                 return $this->render('index', [
                     'notes' => $notes
                 ]);
+                }
     }
 
     /**
