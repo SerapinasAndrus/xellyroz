5f116c3ee8e3799953023c328c58c49e3a0a1b55
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-26 14:09:59 +0300

    notes

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -41,8 +41,8 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-             '' => 'frontend/web/site/index',
-             '<action>'=>'frontend/web/site/<action>',
+             '' => 'site/index',
+             '<action>'=>'site/<action>',
             ],
         ],
 
