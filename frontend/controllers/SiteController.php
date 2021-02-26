d13b229890e06bac71b67b28d57588589f0e69e9
SerapinasAndrus <79200082+SerapinasAndrus@users.noreply.github.com>
2021-02-26 11:50:29 +0300

    notes

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -84,7 +84,7 @@ class SiteController extends Controller
                     $newnotes->name = Yii::$app->request->post()['Notes']['name'];
                     $newnotes->text = Yii::$app->request->post()['Notes']['text'];
                     $newnotes->userid = Yii::$app->user->id;
-                    $newnotes->date = date();
+                    $newnotes->date = date("H:i:s");
                     $newnotes->save();
                     $this->goHome();
                        }
